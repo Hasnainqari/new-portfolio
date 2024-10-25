4dd7849c76aeadfc2698f9bf8ae9100248328424
Hasnainqari <qarihasnain43@gmail.com>
2024-10-25 10:45:37 +0500

    add

--- src/assets/services_data.js
+++ src/assets/services_data.js
@@ -1,34 +1,37 @@
 const Services_Data = [
   {
     s_no: "01",
-    s_name: "Web design",
-    s_desc: "Web development is the process of building, programming...",
+    s_name: "Static Website Development",
+    s_desc:
+      "This service provides flexible, high-speed loading websites that are ideal for displaying information without frequent updates.",
   },
   {
     s_no: "02",
-    s_name: "Graphics design",
-    s_desc: "Web development is the process of building, programming...",
+    s_name: "Ecommerce Website Development",
+    s_desc:
+      "Optimized for online stores, these services include setting up secure, user-friendly methods for inventory, payment and customer management",
   },
   {
     s_no: "03",
-    s_name: "Social media",
-    s_desc: "Web development is the process of building, programming...",
+    s_name: "Web Design",
+    s_desc:
+      "Web design focuses on visually appealing and user-centered design. From color schemes and textures to how devices work.",
   },
-  {
-    s_no: "04",
-    s_name: "App design",
-    s_desc: "Web development is the process of building, programming...",
-  },
-  {
-    s_no: "05",
-    s_name: "Digital marketing",
-    s_desc: "Web development is the process of building, programming...",
-  },
-  {
-    s_no: "06",
-    s_name: "Content writing",
-    s_desc: "Web development is the process of building, programming...",
-  },
+  // {
+  //   s_no: "04",
+  //   s_name: "App design",
+  //   s_desc: "Web development is the process of building, programming...",
+  // },
+  // {
+  //   s_no: "05",
+  //   s_name: "Digital marketing",
+  //   s_desc: "Web development is the process of building, programming...",
+  // },
+  // {
+  //   s_no: "06",
+  //   s_name: "Content writing",
+  //   s_desc: "Web development is the process of building, programming...",
+  // },
 ];
 
 export default Services_Data;
